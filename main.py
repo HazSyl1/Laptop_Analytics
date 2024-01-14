5ced5b65c8e3574bad409ccfc16eedd12a28411f
Sarthak Chaure <sarchaure911@gmail.com>
2024-01-14 14:57:18 +0530

    finished product

--- main.py
+++ main.py
@@ -19,9 +19,13 @@ external_stylesheets=[
 
 df=pd.read_csv("Cleaned_Laptop_data.csv")
 brand_noms=df['brand'].unique().shape[0]
-price_range=f"{df['latest_price'].max():,}-{df['latest_price'].min():,} " 
+price_range=f"{df['latest_price'].min():,} - {df['latest_price'].max():,}" 
 models=df['model'].unique().shape[0]
 os=df['os'].unique().shape[0]
+brands=df['brand'].unique()
+avg_ratings=[round(df[df['brand']==x]['star_rating'].mean()) for x in brands]
+avg_prices=[round(df[df['brand']==x]['latest_price'].mean()) for x in brands]
+
 options=[
     {'label': 'All','value':'All'},
     {'label': 'ASUS', 'value': 'ASUS'}, {'label': 'DELL', 'value': 'DELL'}, {'label': 'Lenovo', 'value': 'Lenovo'}, {'label': 'HP', 'value': 'HP'}, {'label': 'acer', 'value': 'acer'}, {'label': 'MSI', 'value': 'MSI'}, {'label': 'APPLE', 'value': 'APPLE'}, {'label': 'Avita', 'value': 'Avita'}, {'label': 'Vaio', 'value': 'Vaio'}, {'label': 'LG', 'value': 'LG'}, {'label': 'ALIENWARE', 'value': 'ALIENWARE'}, {'label': 'realme', 'value': 'realme'}, {'label': 'Nokia', 'value': 'Nokia'}, {'label': 'lenovo', 'value': 'lenovo'}, {'label': 'Smartron', 'value': 'Smartron'}, {'label': 'MICROSOFT', 'value': 'MICROSOFT'}, {'label': 'Infinix', 'value': 'Infinix'}, {'label': 'RedmiBook', 'value': 'RedmiBook'}, {'label': 'Mi', 'value': 'Mi'}, {'label': 'iball', 'value': 'iball'}, {'label': 'SAMSUNG', 'value': 'SAMSUNG'}
@@ -29,17 +33,21 @@ options=[
 ]
 
 app= dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+server=app.server
 
 #piep=go.Figure(data=[go.Pie(labels=df['brand'].unique() , values=df['brand'].value_counts().tolist())])
 piep=px.pie(labels=df['brand'].unique() , values=df['brand'].value_counts().tolist(),title="Porportion of Market",hole=0.2,names=df['brand'].unique())
 # piep.update_traces(hoverinfo='label+value',textinfo='')
 piep.update_traces(textposition='inside')
 piep.update_layout(uniformtext_minsize=12, uniformtext_mode='hide')
+piep.update_layout(paper_bgcolor='rgba(0,0,0,0)')
 
+three_plot=fig = px.scatter_3d( x=brands, y=avg_prices, z=avg_ratings,color=brands , size_max=12, opacity=0.8)
+three_plot.update_layout(paper_bgcolor='rgba(0,0,0,0)')
 
 app.layout=html.Div([
-    html.H1("Laptop Analysis",style={'color': 'rgb(255, 255, 255)', 'text-align': 'center'}),
+    html.Br(),
+    html.H1("Laptop Analysis",style={'color': 'rgb(255, 255, 255)', 'text-align': 'center' , 'font-family': 'Georgia,serif'}),
     html.Br(),
     html.Div([
         html.Div([
@@ -73,10 +81,12 @@ app.layout=html.Div([
     #PIECHART (MIDDLE ROW)
     html.Div([
        html.Div([
+           dcc.Graph(figure=three_plot,
+                     style={ 'border-radius':'15px', 'background-color':'white'}),
            
            ],className='col-md-8'),
        html.Div([
-           dcc.Graph(figure=piep)
+           dcc.Graph(figure=piep,style={ 'border-radius':'15px', 'background-color':'white'})
            ],className='col-md-4'),
        
         ],className='row'),
@@ -88,7 +98,7 @@ app.layout=html.Div([
                 html.Div([
                     # DROPDOWN
                     dcc.Dropdown(id='picker',options=options ,value='All'),
-                    dcc.Graph(id='bar')
+                    dcc.Graph(id='bar',style={ 'border-radius':'15px', 'background-color':'white'})
                     
                     ],className='card-body')
                 ],className='card')
@@ -105,7 +115,7 @@ def update_graph(type):
         return {'data':[
             go.Bar(x=df['brand'].value_counts().keys().tolist(), y=df['brand'].value_counts().tolist())
             ],
-                'layout':go.Layout(title='Stats')}
+                'layout':go.Layout(title='Stats',paper_bgcolor='rgba(0,0,0,0)')}
     else:
         units=df[df['brand']==type].shape[0]
         model=df[df['brand']==type].model.unique().shape[0]
@@ -121,7 +131,7 @@ def update_graph(type):
         return {'data':[
             go.Bar(x=titles, y=vals)
             ],
-                'layout':go.Layout(title='Brand Stats')}
+                'layout':go.Layout(title='Brand Stats',paper_bgcolor='rgba(0,0,0,0)' )}
         
 
         
